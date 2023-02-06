9f5a871c372c5d45d97886063a21210f7ceac8f4
辣橙 <68316902+laorange@users.noreply.github.com>
2023-02-06 13:57:46 +0800

    feat: 以拖拽的方式调整TextHandler顺序

--- src/pages/article-copy-tool/components/TextHandlerConfig.vue
+++ src/pages/article-copy-tool/components/TextHandlerConfig.vue
@@ -2,9 +2,9 @@
 import {textHandlers, TextHandlerWithName} from "../../../assets/ts/article-copy-tool/handlers";
 import {ref, watch} from "vue";
 import {useStore} from "../../../store/useStore";
+import Draggable from "vuedraggable";
 
 const store = useStore();
-const handlerAmount = store.textHandlerArray.length;
 
 const showConfigDrawer = ref(false);
 const refTextHandlerArray = ref<TextHandlerWithName[]>(store.textHandlerArray);
@@ -23,11 +23,8 @@ function setTextHandlerArrayToDefault() {
   });
 }
 
-function movePositionOfHandler(type: "up" | "down", index: number) {
-  let temp: TextHandlerWithName = refTextHandlerArray.value[index];
-  let indexTarget = type === "up" ? index - 1 : index + 1;
-  refTextHandlerArray.value[index] = refTextHandlerArray.value[indexTarget];
-  refTextHandlerArray.value[indexTarget] = temp;
+function turnOffAllTextHandler() {
+  refTextHandlerArray.value.forEach(th => th.activate = false);
 }
 </script>
 
@@ -38,29 +35,37 @@ function movePositionOfHandler(type: "up" | "down", index: number) {
     </n-badge>
   </div>
 
-  <n-drawer v-model:show="showConfigDrawer" :height="`${Object.keys(textHandlers).length * 40 + 100}px`" placement="bottom">
+  <n-drawer v-model:show="showConfigDrawer" :height="`${Object.keys(textHandlers).length * 50 + 125}px`" placement="bottom">
     <n-drawer-content title="功能设置" :closable="true">
       <n-space justify="center" align="center" :vertical="true">
         <n-space style="height: 100%" :vertical="true" justify="center" align="end" :size="2">
-          <n-form-item v-for="(textHandler, index) of refTextHandlerArray" :key="`textHandler-${textHandler.description}`"
-                       :show-feedback="false" label-placement="left">
-            <template #label>
-              <div style="max-width: calc(100vw - 190px); flex-wrap: wrap">{{ textHandler.description }}</div>
+          <Draggable v-model="refTextHandlerArray" item-key="handlerName">
+            <template #item="{element}">
+              <div class="text-handler-card" @click="element.activate = !element.activate">
+                <n-space :size="10" justify="start">
+                  <n-switch :value="element.activate"/>
+                  <div> {{ element.description }}</div>
+                </n-space>
+              </div>
             </template>
-
-            <n-space :size="10">
-              <n-switch v-model:value="textHandler.activate"/>
-              <n-button round type="success" dashed size="small" @click="movePositionOfHandler(`up`, index)" :disabled="index===0">↑</n-button>
-              <n-button round type="success" dashed size="small" @click="movePositionOfHandler(`down`, index)" :disabled="index===handlerAmount-1">↓</n-button>
-            </n-space>
-          </n-form-item>
+          </Draggable>
         </n-space>
-        <n-button type="warning" size="small" @click="setTextHandlerArrayToDefault()">恢复为默认值</n-button>
+
+        <n-space>
+          <n-button type="info" size="small" @click="setTextHandlerArrayToDefault()">恢复为默认值</n-button>
+          <n-button type="warning" size="small" @click="turnOffAllTextHandler()">全部关闭</n-button>
+          <n-button type="success" size="small" @click="showConfigDrawer=false">完成设置</n-button>
+        </n-space>
       </n-space>
     </n-drawer-content>
   </n-drawer>
 </template>
 
 <style scoped>
-
+.text-handler-card {
+  border: #efefef solid 1px;
+  margin: 5px;
+  padding: 10px 25px;
+  cursor: move;
+}
 </style>
