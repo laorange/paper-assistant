9cc5cdf235eb438f7e4a08db49977ee0921ed0bf
辣橙 <68316902+laorange@users.noreply.github.com>
2023-02-08 15:37:19 +0800

    feat: 自动清空输入、复制输出

--- src/pages/article-copy-tool/components/HeaderArea/components/AdditionalConfig/AdditionalConfig.vue
+++ src/pages/article-copy-tool/components/HeaderArea/components/AdditionalConfig/AdditionalConfig.vue
@@ -1,14 +1,36 @@
 <script setup lang="ts">
 import {useStore} from "../../../../../../store/useStore";
 import ConfigSwitchButton from "./components/ConfigSwitchButton.vue";
+import useClipboard from "vue-clipboard3";
+import {onBeforeUnmount, onMounted} from "vue";
+import {useMessage} from "naive-ui";
 
 const store = useStore();
+const {toClipboard} = useClipboard();
+const message = useMessage();
+
+function onLeavePage() {
+  if (document.hidden) {
+    if (store.storage.copy.copyOutputWhenLeave && store.copy.outputText) {
+      toClipboard(store.copy.outputText).then(() => message.success("复制输出文本: 成功"));
+    }
+    if (store.storage.copy.clearInputWhenLeave) store.copy.inputText = "";
+  }
+}
+
+onMounted(() => {
+  document.addEventListener("visibilitychange", onLeavePage);
+});
+
+onBeforeUnmount(() => {
+  document.removeEventListener("visibilitychange", onLeavePage);
+});
 </script>
 
 <template>
   <n-space justify="center" align="center" wrap="wrap">
     <ConfigSwitchButton label="自动清空输入" v-model:status="store.storage.copy.clearInputWhenLeave"/>
-    <ConfigSwitchButton label="自动复制输入" v-model:status="store.storage.copy.copyOutputWhenLeave"/>
+    <ConfigSwitchButton label="自动复制输出" v-model:status="store.storage.copy.copyOutputWhenLeave"/>
     <ConfigSwitchButton label="实时输出" v-model:status="store.storage.copy.autoOutput"/>
   </n-space>
 </template>
