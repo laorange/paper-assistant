bb29d6a953bb105f93883c828e0de6200d1f2a2b
辣橙 <68316902+laorange@users.noreply.github.com>
2025-01-22 00:03:50 +0800

    chore: 更新ci/cd，format代码
    Co-Authored-By: Lu ZHANG <104704068+VON0000@users.noreply.github.com>

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import {createApp} from "vue";
 import App from "./App.vue";
-import "intro.js/introjs.css"
+import "intro.js/introjs.css";
 import {createPinia} from "pinia";
 import Vue3Storage from "vue3-storage";
 
 createApp(App)
-    .use(createPinia())
-    .use(Vue3Storage, { namespace: "laorange_" })
-    .mount("#app");
+  .use(createPinia())
+  .use(Vue3Storage, {namespace: "laorange_"})
+  .mount("#app");
